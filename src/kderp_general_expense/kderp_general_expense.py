1995f05b4233ab9ad491a318605549ba74bc8a7b
Vu Thu Ha <Vu Thu Ha@VTHa-PC.kinden.com.vn>
2015-06-01 15:42:33 +0700

    [REM] Xoa bo ham _get_expense_type_selection ( de Other Expense hien thi day du type)

--- src/kderp_general_expense/kderp_general_expense.py
+++ src/kderp_general_expense/kderp_general_expense.py
@@ -151,16 +151,8 @@ class kderp_other_expense(osv.osv):
         if context.get('general_expense', False):
             return filter(lambda x: x[0] <> 'PE', self.ALLOCATE_SELECTION)
         else:
-            return filter(lambda x: x[0] <>'GE', self.ALLOCATE_SELECTION)
+            return filter(lambda x: x[0] <>'GE', self.ALLOCATE_SELECTION)   
         
-    def _get_expense_type_selection(self, cr, uid, context):
-        if not context:
-            context = {}   
-        if context.get('general_expense', False):
-            return filter(lambda x: x[0] <> '', self.EXPENSE_TYPE_SELECTION)
-        else:
-            return filter(lambda x: (x[0] <> 'prepaid' and x[0] <> 'fixed_asset'), self.EXPENSE_TYPE_SELECTION) 
-            
     #Get defaults values
     def _get_job(self, cr, uid, context={}):
         if not context:
@@ -315,7 +307,7 @@ class kderp_other_expense(osv.osv):
     
     _columns = {
                 #'allocated_date':fields.date('Allocated Date', states={'paid':[('readonly', True)], 'done':[('readonly',True)], 'cancel':[('readonly',True)]}),
-                'expense_type':fields.selection(_get_expense_type_selection, 'Exp. Type', required = True, states={'paid':[('readonly', True)], 'done':[('readonly',True)], 'cancel':[('readonly',True)]},
+                'expense_type':fields.selection(EXPENSE_TYPE_SELECTION, 'Exp. Type', required = True, states={'paid':[('readonly', True)], 'done':[('readonly',True)], 'cancel':[('readonly',True)]},
                                                 help="""Expense: Allocated direct to Job/General have payment\nRecognize Expense: Recognize allocated to Job/General from Fixed Asset, Prepaid without payment\nPrepaid, Fixed Asset for management and don't allocated"""),
                 'allocated_to':fields.selection(_get_allocated_selection, 'Allocate To', required = True, states={'paid':[('readonly', True)], 'done':[('readonly',True)], 'cancel':[('readonly',True)]}, select = 1),
                 
