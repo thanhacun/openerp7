4cff4624b19162e1e1bbd1322d8a0ddc226b7603
dautienthuy <dautienthuy@gmail.com>
2015-06-16 16:56:06 +0700

    [IMP]Xoa total_qty

--- src/kderp_prepaid_purchase/kderp_prepaid_purchase_line_detail.py
+++ src/kderp_prepaid_purchase/kderp_prepaid_purchase_line_detail.py
@@ -41,7 +41,6 @@ class kderp_prepaid_purchase_order_line_detail(osv.osv):
               'requesting_qty':fields.float('Requesting Qty', digit=(16,2)),
               'product_uom':fields.char('Unit', size = 6),              
               'date':fields.date('Date'),
-              'total_qty':fields.related('prepaid_order_line_id','product_qty',string='Total Quantity', type='float')
               }
         
     def init(self, cr):
