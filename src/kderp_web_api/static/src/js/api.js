fe3ccd013a3e049fec39434646d5a149d44e45bd
THCN <thanhacun@gmail.com>
2016-06-09 15:25:58 +0700

    [IMP] Add hour forecast for weather object (allow update every 1 hour)

--- src/kderp_web_api/static/src/js/api.js
+++ src/kderp_web_api/static/src/js/api.js
@@ -2,7 +2,8 @@ openerp.kderp_web_api = function(instance) {
   //console.log("Helper functions loaded!");
   var apiURL = {
     location: 'http://ip-api.com/json',
-    weather: 'http://thcn-api.herokuapp.com/api/weathers/now/'
+    weather_now: 'http://thcn-api.herokuapp.com/api/weathers/now/',
+    weather_hourly: 'http://thcn-api.herokuapp.com/api/weathers/hourly/'
   }
   instance.web.api = {
     curLoc: function(cb) {
@@ -11,11 +12,23 @@ openerp.kderp_web_api = function(instance) {
       });
     },
     weather: function(lat, lon, cb) {
-      $.getJSON(apiURL.weather + lat + ',' + lon, function(res) {
-        var result = res.current_observation;
+      var res = {};
+      //get weather condition
+      $.getJSON(apiURL.weather_now + lat + ',' + lon, function(weatherNow) {
         var svg_url = 'https://icons.wxug.com/i/c/v4/';
-        result.svg_icon_url = svg_url + result.icon + '.svg';
-        return cb(result);
+        res.now = weatherNow.current_observation;
+        res.now.svg_icon_url = svg_url + res.now.icon + '.svg';
+        //get hourly forcast, only today part, flatten the return object for easily handling QWeb
+        $.getJSON(apiURL.weather_hourly + lat + ',' + lon, function(weatherHourly) {
+          var hourForecast = weatherHourly.hourly_forecast[0];
+          res.forecast = {
+            feelslike_c: hourForecast.feelslike.metric,
+            humidity: hourForecast.humidity,
+            uvi: hourForecast.uvi,
+            pop: hourForecast.pop
+          };
+          return cb(res);
+        });
       });
     }
   };
